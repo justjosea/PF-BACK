28a31c1b9053c7ee1836532416a1147dfdbbdf09
Jose Angel Rodriguez Gonzalez <rodriguezgonzalez@uninorte.edu.co>
2022-11-28 22:46:41 -0500

    changed: purchase model

--- models/purchase.model.js
+++ models/purchase.model.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const purchaseSchema = new mongoose.Schema({
     user: {type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-    products: [{type: mongoose.Schema.ObjectId, ref: 'Product', required: true}],
+    product: {type: mongoose.Schema.ObjectId, ref: 'Product', required: true},
+    amount: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Purchase', purchaseSchema);
